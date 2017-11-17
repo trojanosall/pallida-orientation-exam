ea6bc28e9ec6667d1535aa1a84205f09adb3d1f5
Lorant Lajos <trojanosall@gmail.com>
2017-11-17 10:25:29 +0100

    CarPlate class fill up with properties

--- LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Models/CarPlate.cs
+++ LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Models/CarPlate.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LicencePlateWebApp.Models
+﻿namespace LicencePlateWebApp.Models
 {
     public class CarPlate
     {
+        public int? Id { get; set; }
+        public string Plate { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
     }
 }
