4613b9f9879657f6f1f6007007af3a1e45c248d5
Lorant Lajos <trojanosall@gmail.com>
2017-11-17 13:22:41 +0100

    search what endpoint created

---
+++ LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using LicencePlateWebApp.Models;
+using LicencePlateWebApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LicencePlateWebApp.Controllers
+{
+    public class ApiController : Controller
+    {
+        CarPlateRepository CarPlateRepository;
+
+        public ApiController(CarPlateRepository carPlateRepository)
+        {
+            CarPlateRepository = carPlateRepository;
+        }
+
+        [HttpGet]
+        [Route("/search{what}")]
+        public IActionResult Search([FromBody] CarPlate carPlate, string what)
+        {
+            var thisCarPlate = CarPlateRepository.GetByPlate(what);
+
+            if (thisCarPlate == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(thisCarPlate);
+
+
+        }
+    }
+}
