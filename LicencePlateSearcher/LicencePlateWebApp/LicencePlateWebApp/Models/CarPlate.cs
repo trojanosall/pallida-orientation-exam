8b98d26865253de84e2cce180e6fdeb981ffd892
Lorant Lajos <trojanosall@gmail.com>
2017-11-17 11:00:49 +0100

    Validation in model class is done

--- LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Models/CarPlate.cs
+++ LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Models/CarPlate.cs
@@ -1,8 +1,13 @@
-﻿namespace LicencePlateWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LicencePlateWebApp.Models
 {
     public class CarPlate
     {
         public int? Id { get; set; }
+
+        [MinLength(7, ErrorMessage = "Only allow alphanumeric characters from the user: [A-Z], [0-9] and the - dash symbol and lenghts of the plate is 7 character with dash symbol")]
+        [MaxLength(7)]
         public string Plate { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
