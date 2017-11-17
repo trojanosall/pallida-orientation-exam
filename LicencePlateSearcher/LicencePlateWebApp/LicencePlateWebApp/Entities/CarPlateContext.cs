330897824b35000ff3fa165a66d9a129daef60d7
Lorant Lajos <trojanosall@gmail.com>
2017-11-17 10:13:48 +0100

    Orientation exam - add dependencies + startup.cs update + controllers folder create + homcontroller created + view folder created + entities folder created + context.cs created + rep folder created + repository.cs created

---
+++ LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Entities/CarPlateContext.cs
@@ -0,0 +1,15 @@
+﻿using LicencePlateWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicencePlateWebApp.Entities
+{
+    public class CarPlateContext : DbContext
+    {
+        public CarPlateContext(DbContextOptions<CarPlateContext> options) : base(options)
+        {
+        }
+        public DbSet<CarPlate> CarPlates { get; set; }
+    }
+
+
+}
---
+++ LicencePlateSearcher/LicencePlateWebApp/LicencePlateWebApp/Repositories/CarPlateRepository.cs
@@ -0,0 +1,14 @@
+﻿using LicencePlateWebApp.Entities;
+
+namespace LicencePlateWebApp.Repositories
+{
+    public class CarPlateRepository
+    {
+        CarPlateContext CarPlateContext;
+
+        public CarPlateRepository(CarPlateContext carPlateContext)
+        {
+            CarPlateContext = carPlateContext;
+        }
+    }
+}
